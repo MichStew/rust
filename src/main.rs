c6d0ddd9f5bf602ca33310d02a1616d51bd880f6
Michael Stewart <michael.stewart@amarok.com>
2026-04-16 15:02:14 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// this loooks like c lol 
+//
+//
+//
+
+fn main() {
+    // create test array of 32 bit integer? do I have to define type like this every time? 
+    let mut test: [i32; 6] = [0,1,2,3,4,5]; // I can also do 0; 5?
+    for item in test {
+        print!("{item}"); //usually {} are objects, sometimes dynamic in other languages? 
+    }
+}
---
+++ target/debug/deps/first_rust.d
@@ -0,0 +1,5 @@
+C:\Users\michael.stewart\first_rust\target\debug\deps\first_rust.d: src\main.rs
+
+C:\Users\michael.stewart\first_rust\target\debug\deps\first_rust.exe: src\main.rs
+
+src\main.rs:
