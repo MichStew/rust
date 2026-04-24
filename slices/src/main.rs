256cc0fcd7118a3d4633f7a5fa30b83e270d90d0
Michael Stewart <stew.mich258@gmail.com>
2026-04-24 11:30:44 -0400

    finishing up chapter 4

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,15 +1,30 @@
-fn first_word(s: &String) {
+fn first_word(s: &str) -> &str {
   let bytes = s.as_bytes(); 
 
-  for(i, &item) in bytes.iter.enumerate() {
+  for(i, &item) in bytes.iter().enumerate() {
     if item == b' ' {
-      return i; 
+      return &s[0..i]; // return the slice 
     }
   }
-  s.len() // default return length of s 
+  &s // default return address of string 
 }
 
 
 fn main() {
     println!("Hello, world!");
+    let s = String::from("hello world"); 
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    println!("first word is {}. the second word is {}", hello, world);
+
+    let first = first_word(&s); 
+    println!("{} is the first word, as found by first_word function",first);
+    
+    let a = [1,2,3,4,5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2,3]); // so slice will hold 2 & 3, why do we pass &[2,3] ? just saying
+                               // references 2 and 3? 
+
 }
