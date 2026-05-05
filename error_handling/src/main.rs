10466499d492fc9ef10edb411219784b73021b4b
Michael Stewart <michael.stewart@amarok.com>
2026-05-05 13:03:02 -0400

    error handling : check. Just custom types for error handling is a bit ~fuzzy~

---
+++ error_handling/src/main.rs
@@ -0,0 +1,49 @@
+use std::fs::File; 
+use std::io::ErrorKind;
+
+fn main() {
+    //panic!("crash and burn");
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file_result = match greeting_file_result { // becauese file::open returns T or E 
+        Ok(file) => file, 
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem opening the file: {e:?}"),  
+            },
+        _ => { panic!("Problem opening the file: {error:?}"); } 
+     },
+  };
+}
+
+fn read_username() -> Result<String, io::Error> { // return string or error 
+
+    let username_file_result = File::open("hello.txt");  // returns Ok(file) or Error(e)  
+    let mut username_file = match username_file_result { // depending on what File::open returned 
+      Ok(file) => file, 
+      Err(e) => return Err(e), //propogate the error. 
+    }; 
+    
+    let mut username = String::new();  
+    match username_file.read_to_string(&mut username) { // if we have a username file 
+        Ok(_) => Ok(username), 
+        Err(e) => Err(e),
+    }
+}
+
+// apparently there is a much easier way to achieve the same functionality 
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;  // notice the ?
+    // the ? returns the error or the file. 
+    let mut username = String::new(); 
+
+    username_file.read_to_string(&mut username)?; 
+    Ok(username)  // return username 
+}
+
+// even simpler
+use std::io;
+fn read_username_from_file2() -> Result<String, io::Error {
+    fs::read_to_string("hello.txt");
+}
